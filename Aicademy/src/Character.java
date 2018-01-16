89ddb51bb53a3e4eda9345105ac528d40feccb02
Amber Brownstein <amberbrownstein@gmail.com>
2018-01-15 23:42:24 -0500

    changed public to protected

--- Aicademy/src/Character.java
+++ Aicademy/src/Character.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
 
 public class Character {
-	public String name;				// what the character's name appears to be to the player
-	public int ap = 0;				// the affection point this character has for the player
-	public boolean date;			// whether or not the player can date this character (based on gender)
-	public boolean phone = false;	// if the player has this character's phone number
-	public int dated = 0;			// number of dates the player has had with this character
-	public String previous = "";	// previous dialogue; ensures dialogue does not repeat, provided ap increases
-	boolean intro = false;			// whether or not character's name is established; ensures no repeat of introduction in case of no ap increase
-	Scanner sc;						// this should point to the Scanner declared in the Player class, retrieved from Setting
-	public Setting set;			// gives access to the following variables in the Setting class:
-	public int day;
-	public int time;
-	public int location;
-	public Player user;
+	String name;			// what the character's name appears to be to the player
+	int ap = 0;				// the affection point counter this character has for the player
+	boolean phone = false;	// if the player has this character's phone number
+	int dated = 0;			// number of dates the player has had with this character
+	String previous = "";	// previous dialogue; ensures dialogue does not repeat, provided ap increases
+	boolean intro = false;	// whether or not character's name is established; ensures no repeat of introduction in case of no ap increase
+	Scanner sc;				// this should point to the Scanner declared in the Player class, retrieved from Setting
+	Setting set;			// gives access to the following variables in the Setting class:
+	int day;
+	int time;
+	int location;
+	Player user;
 	
-	public boolean present(int time, int day, int location){return false;}
-	public void speak(int time, int day, int location){}
+	boolean present(int time, int day, int location){return false;}
+	void speak(int time, int day, int location){}
 }
